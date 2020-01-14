e19feb07bca836b56224b043f53e5b4f051ca3ae
jhhest <janvanhest@outlook.com>
2020-01-14 18:46:35 +0100

    Made some pseudo code

---
+++ hangman.js
@@ -0,0 +1,11 @@
+// Fill in the word to guess. Use only Upper caps.
+const guessedWord = "EXAMPLE";
+const guessedLetters = []
+// convert a String to an array.
+
+// replace each letter of string with X. 
+
+// Extract correct guessed letters
+
+// Convert a string to uppercase
+
