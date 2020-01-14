7212685331bb3d1d839b875f7fda0f270fac729b
jhhest <janvanhest@outlook.com>
2020-01-14 19:27:04 +0100

    added function -> replaceLettersArrayWithX

--- hangman.js
+++ hangman.js
@@ -1,11 +1,31 @@
 // Fill in the word to guess. Use only Upper caps.
 const guessedWord = "EXAMPLE";
-const guessedLetters = []
+const guessedWordArray = strToArray(guessedWord);
+const guessedLetters = [];
+const scoreboard = [];
+
 // convert a String to an array.
+function strToArray(str) {
+  const inputStr = str;
+  const result = Array.from(inputStr);
+  return result;
+}
+gi// // test:
+// console.log("Convert a string into an Array:", strToArray(guessedWord));
 
-// replace each letter of string with X. 
+// replace each letter of string with X.
+function replaceLettersArrayWithX(arr) {
+  const inputArr = [...arr];
+  const result = [];
+  inputArr.forEach(element => result.push("X"));
+  return result;
+}
 
+// // test
+// console.log("replaceLettersArrayWithX():", replaceLettersArrayWithX(guessedWordArray));
+
+// TODO:
 // Extract correct guessed letters
 
 // Convert a string to uppercase
-
+// console.log(replaceLettersArrayWithX(guessedWordTransformedToArray));
